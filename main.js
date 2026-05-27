218b6a8d1680e5b1277ecdd6e1604c0b563f30cb
Sandytheking <sandytheking@hotmail.com>
2026-05-27 13:19:20 -0400

    fix: Access-Control-Allow-Private-Network y quitAndInstall corregido

--- main.js
+++ main.js
@@ -258,6 +258,7 @@ function setCorsHeaders(res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Private-Network', 'true')
 }
 
 function parseBody(req) {
